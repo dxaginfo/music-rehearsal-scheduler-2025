b2db69d974c89a462c180203741560b8dbec25ef
dxaginfo <142343166+dxaginfo@users.noreply.github.com>
2025-06-19 06:20:44 -0300

    Add validation middleware

---
+++ backend/src/middleware/validate.js
@@ -0,0 +1,23 @@
+const { validationResult } = require('express-validator');
+
+/**
+ * Middleware to validate request body/params/query using express-validator
+ * @param {Object} req - Express request object
+ * @param {Object} res - Express response object
+ * @param {Function} next - Express next middleware function
+ */
+const validate = (req, res, next) => {
+  const errors = validationResult(req);
+  if (!errors.isEmpty()) {
+    return res.status(400).json({
+      message: 'Validation failed',
+      errors: errors.array().map(error => ({
+        field: error.param,
+        message: error.msg
+      }))
+    });
+  }
+  next();
+};
+
+module.exports = { validate };
